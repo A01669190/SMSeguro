05b980a8c33bf9c331ba00ae1e62517bcb8e134d
Eduardo Arteaga <eduardoart100@gmail.com>
2026-09-14 14:02:25 -0600

    Se anadio estado vacio de label a UI a y links de Outlets en pantalla reportes

---
+++ ReportesViewController.swift
@@ -0,0 +1,32 @@
+//
+//  ReportesViewController.swift
+//  SMSeguro
+//
+//  Created by alumno on 14/09/26.
+//
+
+import UIKit
+
+class ReportesViewController: UIViewController {
+    
+    @IBOutlet weak var stackTarjetas: UIStackView!
+    @IBOutlet weak var lblEstadoVacio: UILabel!
+    
+    override func viewDidLoad() {
+        super.viewDidLoad()
+
+        // Do any additional setup after loading the view.
+    }
+    
+
+    /*
+    // MARK: - Navigation
+
+    // In a storyboard-based application, you will often want to do a little preparation before navigation
+    override func prepare(for segue: UIStoryboardSegue, sender: Any?) {
+        // Get the new view controller using segue.destination.
+        // Pass the selected object to the new view controller.
+    }
+    */
+
+}
